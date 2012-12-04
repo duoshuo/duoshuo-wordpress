415ae28bbfe0eb217b7c96692074609fcefa9f11
shen2 <zhenyupku@gmail.com>
2012-12-04 12:39:10 +0800

    remove autoresize script

--- duoshuo/profile.php
+++ duoshuo/profile.php
@@ -12,17 +12,5 @@ $settingsUrl = 'http://' . self::DOMAIN.'/settings/?' . http_build_query($params
 <h2>我的多说帐号
 	<a class="add-new-h2" target="_blank" href="<?php echo $settingsUrl;?>">在新窗口中打开</a>
 </h2>
-<iframe id="duoshuo-remote-window" src="<?php echo $settingsUrl;?>" style="width:960px;"></iframe>
+<iframe id="duoshuo-remote-window" src="<?php echo $settingsUrl;?>" style="width:960px;height:750px;"></iframe>
 </div>
-
-<script>
-jQuery(function(){
-var $ = jQuery,
-	iframe = $('#duoshuo-remote-window'),
-	resetIframeHeight = function(){
-		iframe.height($(window).height() - iframe.offset().top - 70);
-	};
-resetIframeHeight();
-$(window).resize(resetIframeHeight);
-});
-</script>
