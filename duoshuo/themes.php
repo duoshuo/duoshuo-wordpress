9717f8b85dd8d434b7be095ad8a2230820adfd74
shen2 <zhenyupku@gmail.com>
2013-08-28 23:08:15 +0800

    多说的CSS样式已经开源啦！

--- duoshuo/themes.php
+++ duoshuo/themes.php
@@ -1,12 +1,14 @@
 <link rel="stylesheet" href="<?php echo $this->pluginDirUrl; ?>styles.css" type="text/css" />
 <div class="wrap">
+<a href="https://github.com/duoshuo/duoshuo-embed.css" target="_blank"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub"></a>
 <?php screen_icon(); ?>
-<h2>多说评论主题设置</h2>
+<h2>多说主题设置</h2>
 
 <form action="" method="post">
 <?php wp_nonce_field('duoshuo-local-options');?>
-	<ul class="ds-themes">
-	</ul>
+	<ul class="ds-themes"></ul>
+	<p>多说的CSS样式已经开源啦！ <a href="https://github.com/duoshuo/duoshuo-embed.css" target="_blank">github:duoshuo/duoshuo-embed.css</a></p>
+	<p>你可以打造属于自己的主题，在<a href="http://dev.duoshuo.com/discussion" target="_blank">开发者中心</a>分享你的主题，还有可能被官方推荐哟！</p>
 </form>
 <style>
 .ds-themes li{
